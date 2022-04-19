cdc2e40ebd4e0bd8ff026ea17875dae1fbcf7f6a
ABO HADI <eng92salman.net@gmail.com>
2022-04-19 03:39:16 +0300

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:food_delivery_application/screens/food/popular_food_detail.dart';
 import 'package:food_delivery_application/screens/food/recommended_food_detail.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-
-void main() {
+import 'helper/dependencies.dart'as dep;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
